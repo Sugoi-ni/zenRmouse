f11bffb7dbc11ccf469343794835796b7311b492
Sugoi-ni <arporif@gmail.com>
2026-08-29 19:37:21 +0300

    Fix: single window startup

--- start.bat
+++ start.bat
@@ -1,27 +1,33 @@
 @echo off
 title ZenRmouse
+color 0D
+cls
 echo ===================================
 echo  ZenRmouse - Starting...
 echo ===================================
 
 set ANDROID_HOME=C:\Users\Arif\AppData\Local\Android\Sdk
+set WS_PID=
 
-REM Start server in background
+REM Start server
 echo [*] Starting server...
 cd /d C:\Users\Arif\WiiCtl\server
-start /b cmd /c "node src/index.js > nul 2>&1"
+start "" /B node src/index.js
+for /f "tokens=2" %%a in ('tasklist /fi "imagename eq node.exe" /fo list ^| findstr PID') do set WS_PID=%%a
 
 REM Start Metro
 echo [*] Starting Metro bundler...
 cd /d C:\Users\Arif\WiiCtl\mobile
-call npx expo start --dev-client
+npx expo start --dev-client
 
-REM After Metro stops, set ADB reverse
+REM When Metro stops, cleanup
 echo.
+echo [*] Stopping server...
+if defined WS_PID taskkill /PID %WS_PID% /F >nul 2>&1
+
 echo [*] Setting ADB reverse...
 %ANDROID_HOME%\platform-tools\adb.exe reverse tcp:8081 tcp:8081
 
-REM Open QR page
 echo [*] Opening QR code page...
 start http://localhost:8320
 
