ef3e014d1e87adf7e364439359d392287380763d
Sugoi-ni <arporif@gmail.com>
2026-08-29 19:36:04 +0300

    Single window startup

--- start.bat
+++ start.bat
@@ -1,38 +1,32 @@
 @echo off
+title ZenRmouse
 echo ===================================
-echo  ZenRmouse - Start All
+echo  ZenRmouse - Starting...
 echo ===================================
 
 set ANDROID_HOME=C:\Users\Arif\AppData\Local\Android\Sdk
 
-REM 1. Start Node.js server
+REM Start server in background
 echo [*] Starting server...
 cd /d C:\Users\Arif\WiiCtl\server
-start "Server" cmd /k "node src/index.js"
+start /b cmd /c "node src/index.js > nul 2>&1"
 
-REM 2. Start Metro bundler
+REM Start Metro
 echo [*] Starting Metro bundler...
 cd /d C:\Users\Arif\WiiCtl\mobile
-start "Metro" cmd /k "npx expo start --dev-client"
+call npx expo start --dev-client
 
-REM 3. Wait for Metro to be ready
-echo [*] Waiting for Metro to start (15 seconds)...
-timeout /t 15 /nobreak >nul
-
-REM 4. Set ADB reverse
+REM After Metro stops, set ADB reverse
+echo.
 echo [*] Setting ADB reverse...
 %ANDROID_HOME%\platform-tools\adb.exe reverse tcp:8081 tcp:8081
 
-REM 5. Open QR page in browser
+REM Open QR page
 echo [*] Opening QR code page...
 start http://localhost:8320
 
+echo.
 echo ===================================
-echo  Server:  ws://YOUR_IP:8321
-echo  QR Code: http://YOUR_IP:8320
-echo  Metro:   Running
-echo  ADB:     Reverse set
-echo ===================================
-echo  Open ZenRmouse on your phone!
+echo  Done! App is running on phone.
 echo ===================================
 pause
